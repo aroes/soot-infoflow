0a85ee6258786a345b96485044d1f164a07f99e7
Eric Bodden <eric@bodden.de>
2014-01-09 15:49:05 +0100

    changed ICFGs to a decorator style such that different directions and features can be combined freely

--- src/soot/jimple/infoflow/solver/BackwardsInfoflowCFG.java
+++ src/soot/jimple/infoflow/solver/BackwardsInfoflowCFG.java
@@ -1,12 +1,13 @@
 package soot.jimple.infoflow.solver;
 
 import soot.jimple.toolkits.ide.icfg.BackwardsInterproceduralCFG;
+import soot.jimple.toolkits.ide.icfg.JimpleBasedInterproceduralCFG;
 
 
 public class BackwardsInfoflowCFG extends InfoflowCFG {
 
 	public BackwardsInfoflowCFG() {
-		super(new BackwardsInterproceduralCFG());
+		super(new BackwardsInterproceduralCFG(new JimpleBasedInterproceduralCFG()));
 	}
 	
 }
