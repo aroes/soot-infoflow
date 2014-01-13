6daca7f3ad9724dc62755c435ccbc6fb464c2596
Steven Arzt <Steven.Arzt@cased.de>
2014-01-13 18:06:57 +0100

    fixed a character that failed Jenkins and added some comments and timing outputs

--- src/soot/jimple/infoflow/DefaultBiDiICFGFactory.java
+++ src/soot/jimple/infoflow/DefaultBiDiICFGFactory.java
@@ -8,6 +8,9 @@ package soot.jimple.infoflow;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
@@ -20,18 +23,28 @@ import soot.jimple.toolkits.ide.icfg.OnTheFlyJimpleBasedICFG;
  * Default factory for bidirectional interprocedural CFGs
  * 
  * @author Steven Arzt
- * @author Marc-André Lavadiere
+ * @author Marc-Andre Lavadiere
  */
 public class DefaultBiDiICFGFactory implements BiDirICFGFactory {
-
+	
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    
     @Override
     public IInfoflowCFG buildBiDirICFG(CallgraphAlgorithm callgraphAlgorithm){
     	if (callgraphAlgorithm == CallgraphAlgorithm.OnDemand) {
+    		// Collect all application classes
     		Set<SootMethod> applicationMethods = new HashSet<SootMethod>();
     		for (SootClass sc : Scene.v().getApplicationClasses())
     			for (SootMethod sm : sc.getMethods())
     				applicationMethods.add(sm);
+    		
+    		Scene.v().getOrMakeFastHierarchy();
+    		
+    		// Load all classes on the classpath to signatures
+    		long beforeClassLoading = System.nanoTime();
     		OnTheFlyJimpleBasedICFG.loadAllClassesOnClassPathToSignatures();
+    		logger.info("Class loading took {} seconds", (System.nanoTime() - beforeClassLoading) / 1E9);
+    		
     		return new InfoflowCFG(new OnTheFlyJimpleBasedICFG(applicationMethods));
     	}
         return new InfoflowCFG();
