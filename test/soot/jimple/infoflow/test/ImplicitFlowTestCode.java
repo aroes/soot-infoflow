836e793ba345bbead2f73dba52593b3c66017d8e
Steven Arzt <Steven.Arzt@cased.de>
2014-11-05 22:04:21 +0100

    removed some Java warnings

--- test/soot/jimple/infoflow/test/ImplicitFlowTestCode.java
+++ test/soot/jimple/infoflow/test/ImplicitFlowTestCode.java
@@ -154,7 +154,7 @@ public class ImplicitFlowTestCode {
 			if (tainted == "123")
 				throw new RuntimeException("Secret is 42");
 		}
-		catch (RuntimeException ex) {
+		catch (@SuppressWarnings("unused") RuntimeException ex) {
 			doPublish();
 		}
 	}
@@ -315,7 +315,7 @@ public class ImplicitFlowTestCode {
 		try {
 			conditional();
 		}
-		catch (Exception ex) {
+		catch (@SuppressWarnings("unused") Exception ex) {
 			ConnectionManager cm = new ConnectionManager();
 			cm.publish(ImplicitFlowTestCode.staticDataClass.data.data);
 		}
