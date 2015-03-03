2fc3d6948c2b436237a0c8052df505acf5b14426
Steven Arzt <Steven.Arzt@cased.de>
2015-03-03 10:48:31 +0100

    fixed an NPE

--- src/soot/jimple/infoflow/Infoflow.java
+++ src/soot/jimple/infoflow/Infoflow.java
@@ -669,7 +669,10 @@ public class Infoflow extends AbstractInfoflow {
 		logger.info("Source lookup done, found {} sources and {} sinks.", forwardProblem.getInitialSeeds().size(),
 				sinkCount);
 		
-		taintWrapper.initialize();
+		// Initialize the taint wrapper if we have one
+		if (taintWrapper != null)
+			taintWrapper.initialize();
+		
 		forwardSolver.solve();
 		maxMemoryConsumption = Math.max(maxMemoryConsumption, getUsedMemory());
 		
