6ad6a2c7a8d2f928474890d05eaec3083ff76db4
Steven Arzt <Steven.Arzt@cased.de>
2015-12-17 16:43:03 +0100

    removed an unused import

--- src/soot/jimple/infoflow/solver/fastSolver/IFDSSolver.java
+++ src/soot/jimple/infoflow/solver/fastSolver/IFDSSolver.java
@@ -27,7 +27,6 @@ import heros.solver.PathEdge;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
