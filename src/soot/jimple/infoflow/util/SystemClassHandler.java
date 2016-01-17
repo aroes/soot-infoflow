a2be63ead5b7c308b48f31d613f8366ac249082a
Steven Arzt <Steven.Arzt@cased.de>
2016-01-17 14:52:37 +0100

    fixes #32. Thanks for reporting, Nicola!

--- src/soot/jimple/infoflow/util/SystemClassHandler.java
+++ src/soot/jimple/infoflow/util/SystemClassHandler.java
@@ -1,5 +1,8 @@
 package soot.jimple.infoflow.util;
 
+import soot.RefType;
+import soot.Type;
+
 /**
  * Utility class for checking whether methods belong to system classes
  * 
@@ -16,8 +19,21 @@ public class SystemClassHandler {
 	public static boolean isClassInSystemPackage(String className) {
 		return className.startsWith("android.")
 				|| className.startsWith("java.")
+				|| className.startsWith("javax.")
 				|| className.startsWith("sun.")
 				|| className.startsWith("com.google.");
+	}
+	
+	/**
+	 * Checks whether the type belongs to a system package
+	 * @param type The type to check
+	 * @return True if the given type belongs to a system package, otherwise
+	 * false
+	 */
+	public static boolean isClassInSystemPackage(Type type) {
+		if (type instanceof RefType)
+			return isClassInSystemPackage(((RefType) type).getSootClass().getName());
+		return false;
 	}
 
 }
